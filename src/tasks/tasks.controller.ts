b5e16cca7367135e8b24471b7dde63b9bd6f9ce6
sardor-abdullaev <abdullaevss94@mail.ru>
2025-02-26 11:02:09 +0500

    add create task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private taskService: TasksService) { }
+  constructor(private taskService: TasksService) {}
 
-    @Get()
-    getAllTasks(): Task[] {
-        return this.taskService.getAllTasks();
-    }
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  //   createTask(@Body() body) {
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    // console.log(body);
+    // console.log(title, description);
+
+    return this.taskService.createTask(title, description);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = [];
+  private tasks: Task[] = [];
 
-    getAllTasks(): Task[] {
-        return this.tasks;
-    }
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
 }
