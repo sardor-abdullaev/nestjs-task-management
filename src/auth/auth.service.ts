635a5a846b2732aa645f78a396b8d4265239180d
Sardor Abdullaev <abdullaevss94@mail.ru>
2025-03-09 06:31:56 +0500

    add username unique feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -8,13 +8,22 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 export class AuthService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
-  ) {}
+  ) { }
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
     const user = this.userRepository.create({ username, password });
-    await this.userRepository.save(user);
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      // console.log(error.code);
+      if (error.code === '23505') { //duplicate
+        throw new ConflictException('Username already exists!')
+      } else {
+        throw new InternalServerErrorException()
+      }
+    }
   }
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
