ee19de7cb11e8cf4ef2405b644c6b7ff34e92b42
sardor-abdullaev <abdullaevss94@mail.ru>
2025-03-10 10:05:09 +0500

    implement jwt validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' }), JwtModule.register({ secret: 'TopSecret08', signOptions: { expiresIn: 3600 } }), TypeOrmModule.forFeature([User])],
-  providers: [AuthService],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'TopSecret08',
+      signOptions: { expiresIn: 3600 },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy, PassportModule],
 })
-export class AuthModule { }
+export class AuthModule {}
