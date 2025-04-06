247330fefbecb42a936f833347b634d62c3f0b94
Sardor Abdullaev <abdullaevss94@mail.ru>
2025-04-07 01:13:33 +0500

    add TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,37 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: [`.env.stage.${process.env.STAGE}`] }), TasksModule, TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'aws-0-ap-south-1.pooler.supabase.com',
-      port: 5432,
-      username: 'postgres.tamuyrmnrohhupltfuuu',
-      password: 'RealMadridN1',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
-      // entities: [Task]
-    }), AuthModule],
+    ConfigModule.forRoot({ envFilePath: [`.env.stage.${process.env.STAGE}`] }), TasksModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        synchronize: true,
+        autoLoadEntities: true,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+      })
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'aws-0-ap-south-1.pooler.supabase.com',
+    //   port: 5432,
+    //   username: 'postgres.tamuyrmnrohhupltfuuu',
+    //   password: 'RealMadridN1',
+    //   database: 'task-management',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    //   // entities: [Task]
+    // }),     
+    AuthModule],
 })
 export class AppModule { }
 
