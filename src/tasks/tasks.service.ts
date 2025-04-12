1dc50e3219f9d2d8dd19e20575bcaa7242debe04
Sardor Abdullaev <abdullaevss94@mail.ru>
2025-04-12 22:44:59 +0500

    add minor fix

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,7 +6,7 @@ import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { Repository } from 'typeorm';
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 
 @Injectable()
 export class TasksService {
